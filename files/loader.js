b742714d264db43b3816a91b6e3f9799628045df
Chandra Prakash <cprakashagr@gmail.com>
2016-01-28 00:30:33 +0530

    Ritomis Lov Initial Commit

--- files/loader.js
+++ files/loader.js
@@ -1,58 +1,18 @@
 function loadData() {
     
+    var formData = '<center><form><input type ="text" placeholder="Name"><br><input type ="text" placeholder="Title"><br><input type ="text" placeholder="Message"><br><input type ="email" placeholder="Email"><br><input type ="phone" placeholder="Phone"><br><input type="button" value="Submit"></form></center>';
+    
     //  Summary
-        var elem = document.getElementById('summaryLoading');
-        elem.parentNode.removeChild(elem);
-        document.getElementById('summary').innerHTML = 'Student programmer with 5+ years of experience in Java, C and Android programming with keen interest in research-oriented work. <br><br>Currently working with Snapwiz Inc. on <a target="_blank" href="http://gliderapp.com/">Glider App</a>.';
+    var elem = document.getElementById('summaryLoading');
+    elem.parentNode.removeChild(elem);
+    document.getElementById('summary').innerHTML = '';
     
-    //  MyOpenSourceContributions  
-        var elem = document.getElementById('myOpenSourceContributionsLoading');
-        elem.parentNode.removeChild(elem);
-    
-    //  GitRepos
-    $.getJSON('https://api.github.com/users/cprakashagr/repos',function(data){
-        var elem = document.getElementById('gitReposLoading');
-        elem.parentNode.removeChild(elem);
-        $.each(data, function(key,val){
-            document.getElementById("gitRepos").innerHTML += '<li><strong><em><a target="_blank" href="'+val.html_url+'">'+val.name+'</em></strong></a><br>'+val.description+'</li><br>';
-        });
-    });
-    
-    //  Gists
-    $.getJSON('https://api.github.com/users/cprakashagr/gists',function(data){
-        var elem = document.getElementById('gistsLoading');
-        elem.parentNode.removeChild(elem);
-        $.each(data, function(key,val){
-            document.getElementById("gists").innerHTML += '<li><strong><em><a target = "_blank" href="'+val.html_url+'">'+val.files.filename+'</em></strong></a><br>'+val.description+'</li><br>';
-        });
-    });
-    
-    //  NonGitProjects
-        var elem = document.getElementById('nonGitProjectsLoading');
-        elem.parentNode.removeChild(elem);
-
-    
-    //  Contribute
-        var elem = document.getElementById('contributeLoading');
-        elem.parentNode.removeChild(elem);
-        document.getElementById("contribute").innerHTML = 'Want to help out? Submit a <a target="_blank" href="https://github.com/cprakashagr?tab=repositories">feature request, open an issue, or submit a patch</a>.';
-
-    
-    //  Skills
-        var elem = document.getElementById('skillsLoading');
-        elem.parentNode.removeChild(elem);
-
-    
-    //  Education
-        var elem = document.getElementById('educationLoading');
-        elem.parentNode.removeChild(elem);
-        document.getElementById('education').innerHTML = '<li><strong><em>Bachelor in Technology Hons., Computer Science Engineering</em></strong><br>Lovely Professional University, Phagwara India &nbsp;&nbsp; <h6><span style="text-align:right">GPA: 8.08/10&nbsp; | &nbsp;2011-2015</span></h6></li>';
-
-    
-    //  Hobbies
-        var elem = document.getElementById('hobbiesLoading');
-        elem.parentNode.removeChild(elem);
-        document.getElementById('hobbies').innerHTML = '<li><em>Singing, Surfing Quora, Computer Programming</em></li>';
-
+    //  Feedback
+    var elem = document.getElementById('feedbackLoading');
+    elem.parentNode.removeChild(elem);
+    document.getElementById('feedback').innerHTML = formData;
+}
+function toggleMenu() {
+    console.log('Hi');
 }
 window.onload = loadData;
